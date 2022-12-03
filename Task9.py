d5077afbe66650c9fb52644e29f9c047671f4bba
Kairat <burambaevkairat@mail.ru>
2022-12-03 20:28:55 +0300

    Task9.py and file.txt added

---
+++ Task9.py
@@ -0,0 +1,23 @@
+def inputlistN(inputlistN):
+    boolean = True
+    while boolean:
+        try:
+            Num = int(input(F'{inputlistN}'))
+            Num = list(range(-Num,Num+1))
+            boolean = False
+        except ValueError:
+            print('Введите число!')
+    return Num
+index = open('file.txt', 'r')
+N = inputlistN('Введите N: ')
+list_var = []
+result = 0
+for i in index:
+    if i != ' ':
+        i = int(i)
+        list_var.append(i)
+index.close()
+#import random #(для перемешивания списка)
+#random.shuffle(N) #(для перемешивания списка)
+result = N[list_var[0]] * N[list_var[1]]
+print('Список: ',N,'\nпозиции из file.txt: ',list_var,'\nПроизведение: ',result)
